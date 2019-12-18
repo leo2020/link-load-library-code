3e0ab3b1f13f2302a5dd9965d27f6f62fa30a97d
piginzoo <piginzoo@gmail.com>
2019-12-18 12:56:13 +0800

    update run for support x86

--- chapter4/run.sh
+++ chapter4/run.sh
@@ -1,13 +1,24 @@
+#!/bin/sh
+
 split(){
 	echo "------------------------------------------------------------------------"
 }
 
+is_mac(){
+	os=`uname  -a`	
+	return os =~ "Darwin" 
+}
+
 echo "编译 a.c , b.c:"
-gcc -c a.c b.c
+#gcc -c -m32 -fno-builtin -fno_protect_stack a.c b.c
+gcc -c -fno-stack-protector  -m32 a.c b.c
 
 split
 echo "链接a.o/b.o:"
-ld -lSystem a.o b.o -o ab.o
+# ld -lSystem -m elf_i386 a.o b.o -o ab.o
+#ld -m elf_i386 -fno-stack-protector -shared --entry main a.o b.o -o ab.o
+#ld -static -m elf_i386 -e main -fno-stack-protector b.o a.o -o ab
+ld -static -m elf_i386 -e main b.o a.o -o ab
 
 split
 echo "a.o的段属性"
